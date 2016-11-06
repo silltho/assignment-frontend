318164e3c372fab4f2e421c72b81849781bff9ac
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2016-11-06 17:31:37 +0100

    finished homework

--- 01-modules/src/module.js
+++ 01-modules/src/module.js
@@ -1,15 +1,25 @@
 export function valid(email) {
-
+  var re = /^[a-z]*\.(mmt-)[bm][0-9]{4}@(fh-salzburg.ac.at)$/g;
+  return re.test(email);
 }
 
 export function degreeProgram(email) {
-
+  var re = /[a-z]*(?=-)/g;
+  return re.exec(email)[0].toUpperCase();
 }
 
 export function level(email) {
-
+  var re = /(b)(?=[0-9]{4})|(m)(?=[0-9]{4})/g;
+  var degree = re.exec(email)[0];
+  if(degree == "b") return 'BA';
+  else if( degree == "m") return 'MA';
+  return null;
 }
 
 export function graduationYear(email) {
-
+  var re = /[0-9]{4}(?=@)/;
+  var incomingYear = re.exec(email)[0];
+  var degree = level(email);
+  if(degree == "BA") return parseInt(incomingYear) + 3;
+  else if(degree == "MA") return parseInt(incomingYear) + 2;
 }
