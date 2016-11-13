9ee266ea6322c6d584dc898754342af58e526ab3
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2016-11-13 13:11:34 +0100

    router (static urls, default route)

--- 02-router/src/app.js
+++ 02-router/src/app.js
@@ -2,6 +2,7 @@ import $ from 'jquery'
 import router from './router'
 import homeTpl from './templates/home.hbs'
 import contactTpl from './templates/contact.hbs'
+//import playerTpl from './templates/player.hbs'
 import notFoundTpl from './templates/not-found.hbs'
 
 const $app = $('#app')
@@ -14,11 +15,31 @@ function contact() {
   $app.html(contactTpl())
 }
 
+/*function player(id) {
+  $app.html(playerTpl({id: id}))
+}*/
+
 function notFound() {
   $app.html(notFoundTpl())
 }
 
 router('/', index)
 router('/contact', contact)
+//router('/players/:id', player)
 router('*', notFound)
-router()
+
+let aTags = $('a');
+for(let i = 0; i < aTags.length; i++){
+  aTags[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    router($(e.currentTarget).attr("href"));
+  });
+}
+
+window.addEventListener('load', function(e) {
+  router(window.location.pathname); //handle path after pageload
+})
+
+window.addEventListener('popstate', function(e) {
+  router(e.state.href); //handle path after back or forward button
+})
--- 02-router/src/router.js
+++ 02-router/src/router.js
@@ -1,3 +1,56 @@
-export default function() {
+var routes = [];
 
+export default function(href, handler) {
+  if(handler != undefined) {
+    routes[href] = handler; // register new route
+  } else if(href != undefined) {
+    if(!route(href)) defaultRoute();
+  }
+
+  function route(href) {
+    if(typeof routes[href] == 'function') {
+      window.history.pushState({href: href}, '', href);
+      routes[href](); // route found
+      return true;
+    }
+    return false;
+  }
+
+  function defaultRoute() {
+    if(typeof routes['*'] == 'function') {
+      window.history.pushState({href: '*'}, '', 'notFound');
+      routes['*'](); // show error route
+      return true;
+    }
+    return false;
+  }
+
+  //if (testHrefParams(href)) routes[getHrefWithoutParams(href)] = handler;
+
+  /*if(testHrefParams(href)) {
+   routes[getHrefWithoutParams(href)]({id: getHrefParamValue(href)})
+   // /player/:id
+   // /player/1234
+   }*/
+
+  /*function getHrefWithoutParams(href){
+    var regex = /.*(?=\/:)/;
+    return href.exec(regex);
+  }
+
+  function getHrefParam(href){
+    var regex = /.*(?=\/:)/;
+    return href.exec(regex);
+  }
+
+  function getHrefParamValue(href){
+    var tmp = getHrefWithoutParams(href);
+    var regex = new RegExp('(?:'+ tmp +').*');
+    return href.exec(regex);
+  }
+
+  function testHrefParams(href){
+    var regex = /(?=\/:\w*)/;
+    return href.test(regex);
+  }*/
 }
