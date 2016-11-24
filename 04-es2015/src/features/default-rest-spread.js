da1a67c299604dd8463917bc8fa91a49da5dfb5b
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2016-11-24 13:28:04 +0100

    implement default-rest-spread tests

--- 04-es2015/src/features/default-rest-spread.js
+++ 04-es2015/src/features/default-rest-spread.js
@@ -36,4 +36,21 @@ export function es5() {
 }
 
 export function es6() {
+  function defaults(x = 12){
+    return x
+  }
+
+  function rest(...x){
+    return x.join(',')
+  }
+
+  function spread(x, y = 0, z = 0) {
+    return x + y +z;
+  }
+
+  return {
+    defaults: defaults,
+    rest: rest,
+    spread: spread
+  }
 }
--- 04-es2015/test/features/default-rest-spread.js
+++ 04-es2015/test/features/default-rest-spread.js
@@ -15,14 +15,14 @@ export default function() {
     o.spread([1, 2]).should.eql(3)
   })
 
-  it.skip('should show default,rest and spread syntax in ES6', () => {
+  it('should show default,rest and spread syntax in ES6', () => {
     var o = es6()
     o.defaults().should.eql(12)
     o.defaults(5).should.eql(5)
 
     o.rest('x', 'y', 'z').should.eql('x,y,z')
 
-    o.spread([1, 2, 3]).should.eql(6)
-    o.spread([1, 2]).should.eql(3)
+    o.spread(...[1, 2, 3]).should.eql(6)
+    o.spread(...[1, 2]).should.eql(3)
   })
 }
